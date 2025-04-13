9e5115983b59b78207138aaa7c05b9ec2ede5b67
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2025-04-13 16:14:42 +0900

    Update bluetooth.c

--- software/mcu_test/multiTask/bluetooth.c
+++ software/mcu_test/multiTask/bluetooth.c
@@ -4,6 +4,7 @@
 #include "queue.h"
 #include "main.h"
 #include "cmsis_os2.h"
+#include "motor.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -12,6 +13,7 @@
 
 extern QueueHandle_t motorQueueHandle;
 extern UART_HandleTypeDef huart2; // debug
+extern uint8_t current_motor_cmd;
 
 #define RX_BUFFER_SIZE 64
 
@@ -19,7 +21,7 @@ static char rx_buffer[RX_BUFFER_SIZE];
 static uint8_t rx_index = 0;
 static uint8_t rx_byte;
 
-static uint16_t global_motor_speed = 800;  // 기본 속도
+static uint16_t global_motor_speed = 450;  // 기본 속도
 
 void Bluetooth_Init(void)
 {
@@ -43,6 +45,10 @@ void Bluetooth_RxCallback(void)
 
 void Parse_Command(const char* cmd)
 {
+	char msgs[64];
+	snprintf(msgs, sizeof(msgs), "[DEBUG] Set speed: %d\r\n", global_motor_speed);
+	HAL_UART_Transmit(&huart2, (uint8_t*)msgs, strlen(msgs), HAL_MAX_DELAY);
+
     char c = cmd[0];
     uint8_t msg;
 
@@ -74,13 +80,13 @@ void Parse_Command(const char* cmd)
            HAL_UART_Transmit(&huart2, (uint8_t*)"[BLE_CMD]: Stop\r\n", 16, HAL_MAX_DELAY);
            break;
         case 'a':
-            global_motor_speed = 800;
+            global_motor_speed = 200;
             break;
         case 'e':
-            global_motor_speed = 900;
+            global_motor_speed = 450;
             break;
         case 'i':
-            global_motor_speed = 1000;
+            global_motor_speed = 600;
             break;
         default:
         {
@@ -89,11 +95,20 @@ void Parse_Command(const char* cmd)
             HAL_UART_Transmit(&huart2, (uint8_t*)err_msg, strlen(err_msg), HAL_MAX_DELAY);
             return;
         }
+
     }
 
     // 속도 설정 메시지 출력 (속도 명령어일 때만)
     if (c == 'a' || c == 'e' || c == 'i')
     {
+    	switch (current_motor_cmd)
+    	{
+			case 'F': Motor_Forward(global_motor_speed); break;
+			case 'B': Motor_Backward(global_motor_speed); break;
+			case 'L': Motor_Left(global_motor_speed); break;
+			case 'R': Motor_Right(global_motor_speed); break;
+    	}
+
         char ok_msg[64];
         snprintf(ok_msg, sizeof(ok_msg), "[BLE] Speed set: %d (CMD: %c)\r\n", global_motor_speed, c);
         HAL_UART_Transmit(&huart2, (uint8_t*)ok_msg, strlen(ok_msg), HAL_MAX_DELAY);
