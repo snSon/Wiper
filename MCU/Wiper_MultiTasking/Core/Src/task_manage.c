c52ea6e214b0096ae4c67f7630b934824752031c
snSon <sjseok9909@gmail.com>
2025-04-22 17:18:34 +0900

    debug spi transmit

--- MCU/Wiper_MultiTasking/Core/Src/task_manage.c
+++ MCU/Wiper_MultiTasking/Core/Src/task_manage.c
@@ -157,14 +157,19 @@ void UltrasonicTask3(void *argument)
 void StartSPITask(void *argument)
 {
     uint8_t rx_val = 0;
-    uint8_t tx_val = 0x5A;
+    uint8_t tx_val = 0x5A;  // Jetson에게 줄 응답 (필요시 수정 가능)
     char msg[64];
 
     for(;;)
     {
         if (HAL_SPI_TransmitReceive(&hspi1, &tx_val ,&rx_val, 1, HAL_MAX_DELAY) == HAL_OK)
         {
-            snprintf(msg, sizeof(msg), "[SPI] 수신: 0x%02X, 응답: 0x%02X\r\n", rx_val, tx_val);
+            // 받은 데이터 해석
+            uint8_t human     = (rx_val >> 2) & 0x01;
+            uint8_t red_light = (rx_val >> 1) & 0x01;
+            uint8_t car       =  rx_val       & 0x01;
+
+            snprintf(msg, sizeof(msg), "[SPI 수신] 사람:%d, 신호:%d, 차량:%d\r\n", human, red_light, car);
         }
         else
         {
@@ -172,9 +177,10 @@ void StartSPITask(void *argument)
         }
 
         HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-        osDelay(DURATION/10);  // 100ms 주기 (또는 원하시는 주기로 조절)
+        osDelay(100); // 100ms 주기
     }
 }
+
 
 void StartLineTracerTask(void *argument)
 {
