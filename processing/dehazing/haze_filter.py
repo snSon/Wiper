6e6d3061c61ecaa86016f4419e9b09f298ba362f
snSon <sjseok9909@gmail.com>
2025-05-14 17:26:24 +0900

    feat : add image&video hazing test

--- processing/dehazing/haze_filter.py
+++ processing/dehazing/haze_filter.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def apply_fog(image, beta=0.1, A=0.9):
+def apply_fog(image, beta=0.5, A=0.9):
     h, w = image.shape[:2]
     depth = np.linspace(1, 3, w)  # 단순 깊이맵 시뮬레이션
     depth_map = np.tile(depth, (h, 1))
